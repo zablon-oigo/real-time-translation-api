66bda0e7ac525eb8daeffb256b39cb0e9bcc1505
zablon <zablonoigo51@gmail.com>
2025-04-21 18:39:57 +0300

    add middleware

--- api/main.py
+++ api/main.py
@@ -6,6 +6,13 @@ from fastapi.templating import Jinja2Templates
 
 app=FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 templates=Jinja2Templates(directory="templates")
 @app.get('/index', response_class=HTMLResponse)
 def index(request:Request):
